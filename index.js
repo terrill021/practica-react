67b82d2f245824c0b3ed15e0925d2d4bfaf3c93d
SURAMERICANA\alejtedu <alejandro.terrill@ceiba.com.co>
2018-10-30 19:01:25 -0500

    proyecto base

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hola mundo!!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('main');
+const componente  = <h1> Hola mundo!! </h1>;
+ReactDOM.render(componente, app);
